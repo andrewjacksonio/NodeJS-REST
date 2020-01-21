48e15a80395598b8f50ee9e49f81f6cf87c5a44b
andrewjacksonio <jackson.andrew@gmail.com>
2020-01-21 13:44:17 +0800

    Changes to pipeline and docker-compose

--- test/tests.js
+++ test/tests.js
@@ -3,21 +3,21 @@ var request = require('request');
 var port = process.env.PORT || 3000;
 
 it('Main page content', function(done) {
-    request('http://localhost:' + port , function(error, response, body) {
+    request('http://api:' + port , function(error, response, body) {
         expect(body).to.equal('Hello World');
         done();
     });
 });
 
 it('Main page status', function(done) {
-    request('http://localhost:' + port , function(error, response, body) {
+    request('http://api:' + port , function(error, response, body) {
         expect(response.statusCode).to.equal(200);
         done();
     });
 });
 
 it('About page content', function(done) {
-    request('http://localhost:' + port + '/about' , function(error, response, body) {
+    request('http://api:' + port + '/about' , function(error, response, body) {
         expect(response.statusCode).to.equal(404);
         done();
     });
